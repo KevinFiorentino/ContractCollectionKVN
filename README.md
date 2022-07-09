187eb1cb74c578dd351efd9e2fadd248d5bcb038
Kevin <kevinfiorentino.1294@gmail.com>
2022-07-08 23:15:21 -0300

    ERC721Enumerable implement

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 - npm install
 - npx hardhat compile
+
+#### CollectionKVN - NFT
+
+- npx hardhat compile
+- npx hardhat run scripts}/CollectionKVN.deploy.js --network rinkeby
--- contracts/CollectionKVN.sol
+++ contracts/CollectionKVN.sol
@@ -3,9 +3,26 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CollectionKVN is ERC721 {
+contract CollectionKVN is ERC721, ERC721Enumerable {
 
-  constructor() ERC721("CollectionKVN", "KVNFT") {}
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("CollectionKVN", "KVNFT") {
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(msg.sender, tokenId);
+        _tokenIdCounter.increment();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 
 }
