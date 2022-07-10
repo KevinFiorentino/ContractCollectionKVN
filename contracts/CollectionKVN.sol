b292ac688fbe9c502be2eb127caa6ea6c4cb6fa1
Kevin <kevinfiorentino.1294@gmail.com>
2022-07-10 00:05:14 -0300

    Calculate DNA Contract

--- contracts/CollectionKVN.sol
+++ contracts/CollectionKVN.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "./CalculateDNA.sol";
 
 contract CollectionKVN is ERC721, ERC721Enumerable {
 
